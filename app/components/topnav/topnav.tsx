07c30485a93e2b65a36f1f7a5783666883b8016b
mouakiang <mouakkia@gmail.com>
2024-05-15 16:10:35 -0400

    Nav links change color when hovered

--- app/components/topnav/topnav.tsx
+++ app/components/topnav/topnav.tsx
@@ -3,10 +3,16 @@ export default function TopNav() {
     return (
         <main>
             <div>
-                <div className="flex flex-row justify-end space-x-5 pt-5 text-white pr-20 md:h-auto">
-                    <Link href="/home">HOME</Link>
-                    <Link href="/about">ABOUT</Link>
-                    <Link href="/projects">PROJECTS</Link>
+                <div className="flex flex-row justify-end space-x-5 pt-5 text-white pr-20 md:h-auto ">
+                    <Link href="/home" className="hover:text-blue-700">
+                        HOME
+                    </Link>
+                    <Link href="/about" className="hover:text-blue-700">
+                        ABOUT
+                    </Link>
+                    <Link href="/projects" className="hover:text-blue-700">
+                        PROJECTS
+                    </Link>
                 </div>
                 <div className="my-4 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-100 dark:via-neutral-50"></div>
             </div>
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -5,39 +5,41 @@ import { sofia, playfair, rainbow } from '../components/font/fonts';
 
 export default function Home() {
     return (
-        <main>
-            <div className="bg-gradient-to-t from-cyan-50 to-cyan-300 h-screen">
-                <div>
-                    <TopNav />
-                </div>
-                <div className="mt-60 mb-40 flex flex-col md:flex-row justify-center md:items-start items-center">
+        <>
+            <main>
+                <div className="bg-gradient-to-t from-cyan-50 to-cyan-300 h-screen">
                     <div>
-                        <p
-                            className={`${sofia.className} text-blue-700 text-8xl md:text-8xl mr-4`}
-                        >
-                            Hello,{' '}
-                        </p>
+                        <TopNav />
                     </div>
-                    <div className="flex flex-col items-center">
-                        <p
-                            className={`${playfair.className} text-white text-3xl md:text-3xl mt-4`}
-                        >
-                            my name is Kia. I am a <br />
-                        </p>
-                        <p
-                            className={`${rainbow.className} text-white text-4xl tracking-wide rmd:text-4xl pl-2`}
-                        >
-                            Software Engineer.
-                        </p>
-                        <div className="mt-4">
-                            <ResumeButton />
+                    <div className="mt-60 mb-40 flex flex-col md:flex-row justify-center md:items-start items-center">
+                        <div>
+                            <p
+                                className={`${sofia.className} text-blue-700 text-8xl md:text-8xl mr-4`}
+                            >
+                                Hello,{' '}
+                            </p>
+                        </div>
+                        <div className="flex flex-col items-center">
+                            <p
+                                className={`${playfair.className} text-white text-3xl md:text-3xl mt-4`}
+                            >
+                                my name is Kia. I am a <br />
+                            </p>
+                            <p
+                                className={`${rainbow.className} text-white text-4xl tracking-wide rmd:text-4xl pl-2`}
+                            >
+                                Software Engineer.
+                            </p>
+                            <div className="mt-4">
+                                <ResumeButton />
+                            </div>
                         </div>
                     </div>
+                    <div className="flex justify-center pt-40">
+                        <LearnMore />
+                    </div>
                 </div>
-                <div className="flex justify-center pt-40">
-                    <LearnMore />
-                </div>
-            </div>
-        </main>
+            </main>
+        </>
     );
 }
